e1d41d6c0b6d791a6e5d1c7943786ce85869ea99
Michael <michaelhdg03@gmail.com>
2021-05-12 13:30:04 -0500

    Travis build user input workaround

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,18 @@
 #include "Deck.h"
 #include "GameManager.h"
 
-int main() {
+int main(int argc, char* argv[]) {
 
+    // TODO: Do this right
+    // Early exit for build server
+    if(argc > 1 && argv[1])
+    {
+        return 0;
+    }
+
+    // Game
     GameManager* Manager = new GameManager();
     delete Manager;
-
 
     return 0;
 }
